13aab5559552867e16e37e9c90ca909315458b32
Brennan Ashton <bashton@brennanashton.com>
2021-04-11 00:33:26 -0700

    Wire up support for ltp

--- autonx.py
+++ autonx.py
@@ -1,9 +1,13 @@
 import attr
 import logging
 import re
+import subprocess
+import datetime
 
 from labgrid.driver.common import Driver
+from labgrid.driver.exception import ExecutionError
 from labgrid.driver.commandmixin import CommandMixin
+from labgrid.driver.externalconsoledriver import ExternalConsoleDriver
 from labgrid.factory import target_factory
 from labgrid.protocol import PowerProtocol, CommandProtocol, ConsoleProtocol
 from labgrid.step import step
@@ -21,17 +25,19 @@ class NSHDriver(CommandMixin, Driver, CommandProtocol):
         boot_expression (str): string to search for on NSH start
     """
 
-    bindings = {"console": ConsoleProtocol, }
+    bindings = {
+        "console": ConsoleProtocol,
+    }
     login_timeout = attr.ib(default=60, validator=attr.validators.instance_of(int))
-    boot_expression = attr.ib(default="NuttShell \(NSH\) NuttX", validator=attr.validators.instance_of(str))
+    boot_expression = attr.ib(
+        default="NuttShell \(NSH\) NuttX", validator=attr.validators.instance_of(str)
+    )
     prompt = attr.ib(default="nsh> ", validator=attr.validators.instance_of(str))
     init_commands = attr.ib(default=attr.Factory(tuple), converter=tuple)
 
     def __attrs_post_init__(self):
         super().__attrs_post_init__()
-        self.re_vt100 = re.compile(
-            r'(\x1b\[|\x9b)[^@-_a-z]*[@-_a-z]|\x1b[@-_a-z]'
-        )
+        self.re_vt100 = re.compile(r"(\x1b\[|\x9b)[^@-_a-z]*[@-_a-z]|\x1b[@-_a-z]")
         self.logger = logging.getLogger("{}:{}".format(self, self.target))
         self._status = 0
 
@@ -47,13 +53,19 @@ class NSHDriver(CommandMixin, Driver, CommandProtocol):
 
         # wait until NuttX has reached it's prompt
         self.console.expect(self.prompt)
-        for command in self.init_commands:  #pylint: disable=not-an-iterable
+        for command in self.init_commands:  # pylint: disable=not-an-iterable
             out, _, err = self._run(command)
             if err != 0:
-                self.logger.error("Unexpected error (%d): %s", err, '\n'.join(out))
+                self.logger.error("Unexpected error (%d): %s", err, "\n".join(out))
 
-
-    def _run(self, cmd: str, *, timeout: int = 30, codec: str = "utf-8", decodeerrors: str = "strict"):  # pylint: disable=unused-argument,line-too-long
+    def _run(
+        self,
+        cmd: str,
+        *,
+        timeout: int = 30,
+        codec: str = "utf-8",
+        decodeerrors: str = "strict"
+    ):  # pylint: disable=unused-argument,line-too-long
         """
         If NSH is in Command-Line mode: Run command cmd and return it's
         output. We really do not have a great way to use echo markers
@@ -71,18 +83,36 @@ class NSHDriver(CommandMixin, Driver, CommandProtocol):
         self.console.sendline(cmp_command)
         _, before, _, _ = self.console.expect(self.prompt, timeout=timeout)
 
-        data = self.re_vt100.sub(
-            '', before.decode('utf-8'), count=1000000
-        ).replace("\r", "").split("\n")
+        data = (
+            self.re_vt100.sub("", before.decode("utf-8"), count=1000000)
+            .replace("\r", "")
+            .split("\n")
+        )
         data = data[1:-1]
 
         # We do not have a good way to look for an error code,
         # but this pattern usually means we at least hit an nsh error at least
-        r_not_found = re.compile(r'nsh: .*: ')
-        if r_not_found.match(data[-1]):
-            return (data, [], 1)
+        r_not_found = re.compile(r"nsh: .*: ")
 
-        return (data, [], 0)
+        try:
+            cmp_command = "{cmd}".format(cmd="echo $?")
+            self.console.sendline(cmp_command)
+            _, before, _, _ = self.console.expect(self.prompt, timeout=timeout)
+
+            ret_data = (
+                self.re_vt100.sub("", before.decode("utf-8"), count=1000000)
+                .replace("\r", "")
+                .split("\n")
+            )
+            return (data, [], int(ret_data[-2]))
+        except:
+            # We do not support $? for finding the return code
+            pass
+
+        if len(data) and r_not_found.match(data[-1]):
+            rsp = (data, [], 255)
+
+        return (data, [], -1)
 
     def on_activate(self):
         """Activate the NSHDriver
@@ -98,7 +128,7 @@ class NSHDriver(CommandMixin, Driver, CommandProtocol):
         self._status = 0
 
     @Driver.check_active
-    @step(args=['cmd'], result=True)
+    @step(args=["cmd"], result=True)
     def run(self, cmd, timeout=30.0, codec="utf-8", decodeerrors="strict"):
         return self._run(cmd, timeout=timeout, codec=codec, decodeerrors=decodeerrors)
 
@@ -114,7 +144,42 @@ class NSHDriver(CommandMixin, Driver, CommandProtocol):
 @attr.s(eq=False)
 class NSHStrategy(ShellStrategy):
     """NSHStrategy - Strategy to switch to shell"""
+
     bindings = {
         "power": PowerProtocol,
         "shell": NSHDriver,
     }
+
+
+@target_factory.reg_driver
+@attr.s(eq=False)
+class SimConsoleDriver(ExternalConsoleDriver):
+    def _read(self, size: int = 1024, timeout: int = 0):
+        """
+        Reads 'size' bytes from the simulator console
+        Keyword Arguments:
+        size -- amount of bytes to read, defaults to 1024
+        """
+        if self._child.poll() is not None:
+            raise ExecutionError("child has vanished")
+
+        pending = self._child.stdout.read(size)
+        if pending is not None:
+            return pending
+        if self._poll.poll(timeout):
+            res = self._child.stdout.read(size)
+            if res is not None:
+                return res
+
+        return b""
+
+    def close(self):
+        """Stops the subprocess, does nothing if it is already closed"""
+        # The simulation does not respond to term, so just kill
+        self._child.kill()
+        outs, errs = self._child.communicate()
+
+        if outs:
+            self.logger.info("child stdout while closing: %s", outs)
+        if errs:
+            self.logger.warning("child error while closing: %s", errs)
--- test/lm3s6965evb/test_qemu.py
+++ test/lm3s6965evb/test_qemu.py
@@ -1,4 +1,3 @@
-
 import pytest
 
 from labgrid import Environment
@@ -13,23 +12,30 @@ import re
 ConsoleLoggingReporter.start(".")
 logging.getLogger().setLevel(logging.DEBUG)
 
-@pytest.fixture(scope='session')
-def target():
-    return Environment(os.path.join(os.path.dirname(__file__), "config.yaml")).get_target('lm3s6965evb-flat')
 
-@pytest.fixture(scope='session')
+@pytest.fixture(scope="session")
+def target():
+    return Environment(
+        os.path.join(os.path.dirname(__file__), "config.yaml")
+    ).get_target("lm3s6965evb-flat")
+
+
+@pytest.fixture(scope="session")
 def strategy(target):
     return target.get_driver(NSHStrategy)
 
-@pytest.fixture(scope='function')
+
+@pytest.fixture(scope="function")
 def shell_command(target, strategy):
-    strategy.transition('shell')
+    strategy.transition("shell")
     return target.get_active_driver(CommandProtocol)
 
+
 def test_echo(shell_command):
-    result = shell_command.run_check('echo OK')
-    assert 'OK' in result
+    result = shell_command.run_check("echo OK")
+    assert "OK" in result
+
 
 def test_version(shell_command):
-    result = shell_command.run_check('cat /proc/version')
-    assert re.match(r'NuttX version \d+\.\d+\.\d+.*', result[0])
+    result = shell_command.run_check("cat /proc/version")
+    assert re.match(r"NuttX version \d+\.\d+\.\d+.*", result[0])
